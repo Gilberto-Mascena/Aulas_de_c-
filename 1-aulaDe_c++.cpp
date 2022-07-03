7c3ab36ad99da6192b25bfbe0cbe3140b40935ec
Gilberto Mascena <gilbertomascena@gmail.com>
2022-07-02 23:34:04 -0300

    Aulas de c++

---
+++ 1-aulaDe_c++.cpp
@@ -0,0 +1,9 @@
+#include  <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Eu estou aprendendo c++ na Udemy." ;
+    return 0;
+}
---
+++ 2-variaveis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main ()
+{
+    int idade;
+    idade = 20;
+    idade++;
+    idade -= 3;
+    cout << idade;
+    return 0; 
+}
